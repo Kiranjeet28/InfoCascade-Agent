4f6a0a07ebe3f08ce96a1fac2190b9c5e321c727
Kiranjeet Kour <kiranjeetkour144@gmail.com>
2026-07-02 20:39:59 +0530

    db update

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,7 @@ export async function connectDB() {
         await mongoose.connect(process.env.MONGODB_URI!);
 
         console.log("✅ Connected to MongoDB");
+        console.log("Database:", mongoose.connection.db?.databaseName);
     } catch (error) {
         console.error("❌ MongoDB connection failed:", error);
         process.exit(1);
